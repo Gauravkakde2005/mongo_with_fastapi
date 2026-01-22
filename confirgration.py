0b04d3933e1bf7ae3a580ac617e9520032d1e33d
GauravKakde2005 <gauravkakde591@gmail.com>
2026-01-22 22:41:30 +0530

    done

--- confirgration.py
+++ confirgration.py
@@ -1,8 +1,11 @@
 
+import os
+from flask.cli import load_dotenv
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://gauravkakde1215y_db_user:gaurav_2005@cluster0.ilxn56l.mongodb.net/?appName=Cluster0"
+load_dotenv()
+uri = os.getenv("MONGODBURL")
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
